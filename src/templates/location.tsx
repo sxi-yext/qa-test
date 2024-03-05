31a0cc5227882fd5dc71aecf3ebda8c83249478d
Suli Xi <sxi@yext.com>
2024-03-05 16:49:28 -0500

    change url field

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -35,7 +35,7 @@ import BreadCrumbs from "../components/Breadcrumbs";
  * Required when Knowledge Graph data is used for a template.
  */
 export const config: TemplateConfig = {
-  pageUrlField: "c_url_text",  
+  pageUrlField: "c_url_url_1",  
   stream: {
     $id: "location-stream",
     // Defines the scope of entities that qualify for this stream.
